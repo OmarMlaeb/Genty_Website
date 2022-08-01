d5d654d03d45bb841f192683cc4f2aa9dd66acf6
Omar Malaeb <omarmlb002@gmail.com>
2022-08-01 18:48:42 +0300

    set up layout

--- website_front-end/src/components/Layout.jsx
+++ website_front-end/src/components/Layout.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+import Routes from '../routes/Routes.jsx';
+
 const Layout = () => {
     return (
-        <div>
-            Layout
-        </div>
+        <BrowserRouter>
+            <Route render={ props => (
+                <div>
+                    <div className="container">
+                        <Header {...props}/>
+                        <div className="main">
+                            <Routes/>
+                        </div>
+                    </div>
+                    <Footer/>
+                </div>
+            )}/>
+        </BrowserRouter>
     )
 }
 
